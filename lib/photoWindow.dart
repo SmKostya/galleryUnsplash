c8b33f1cb346dcb20816f5936fcafff3b3855f79
SmKostya <sm.kostya99@gmail.com>
2020-03-22 03:33:01 +0200

    fix2

--- lib/photoWindow.dart
+++ lib/photoWindow.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gallery_unsplash/gallery.dart';
 
-
 class OpenPhoto extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,11 +11,11 @@ class OpenPhoto extends StatelessWidget {
       ),
       home: Scaffold(
           body: InkWell(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child:SizedBox.expand( 
-              child: new Container(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: SizedBox.expand(
+            child: new Container(
                 alignment: Alignment.bottomLeft,
                 padding: new EdgeInsets.only(left: 20.0, bottom: 16.0),
                 decoration: new BoxDecoration(
@@ -31,7 +30,7 @@ class OpenPhoto extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                       fontSize: 20.0,
                     )))),
-          )),
+      )),
     );
   }
 }
