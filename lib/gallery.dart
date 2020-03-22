74df97bd045d1a562e3c70ca192ddb364c37bece
SmKostya <sm.kostya99@gmail.com>
2020-03-22 03:47:06 +0200

    fix 3

--- lib/gallery.dart
+++ lib/gallery.dart
@@ -75,7 +75,7 @@ class GalleryPageState extends State<GalleryPage> {
   _loadImages() async {
     final response = await http.get(
         "https://api.unsplash.com/photos/?client_id=cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0");
-    if (response.statusCode == 200) {
+    if (response.statusCode == 200 && response.statusCode != null) {
       var allData = json.decode(response.body);
       allData.forEach((dynamic val) {
         var record = PhotoInGallery(
